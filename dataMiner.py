520c94ff1f66b882e1133e2f20ce2c7d2fe72d2f
gpfernandezflorio <gpfernandezflorio@gmail.com>
2016-10-13 16:59:08 -0300

    q onda?

--- dataMiner.py
+++ dataMiner.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Ir_t: Índice de relación en un trial = 1/tiempo.
-# Ir_uxc: Índice de relación de un usuario respecto a una de las cuatro clases = Sum Ir de los trials de ese usuario en los que eligió esa clase.
+# Ir: Índice de relación en un trial = 1/tiempo.
+# Ir_uxc: Índice de relación de un usuario respecto a una de las cuatro clases = Sum Ir de los trials de ese usuario en los que eligió la clase c.
+# Ir_cxt: Índice de relación de un choice respecto a un target = Sum Ir de los trials con el target t en los que se eligió el choice c.
 # Ir_l1xl2: Índice de relación de un logo respecto a otro = Sum Ir de los trials con target l1 en los que se eligió a l2.
 
-# Irr_uxc: Índice de relación reativa de un usuario respecto a una de las cuatro clases = Ir_uxc / cantidad de trials que el usuario eligió esa clase.
+# Irr_uxc: Índice de relación reativa de un usuario respecto a una de las cuatro clases = Ir_uxc / cantidad de trials que el usuario eligió la clase c.
+# Irr_cxt: Índice de relación reativa de un choice respecto a un target = Ir_cxt / cantidad de veces que apareció el choice c
+
+
+# Ordenar el randoms por tamaño de globos.
+# El de pTarget, hacerlo de dos dimensiones: En el eje y poner las 4 clases y en el eje z (color/tamaño) el índice NORMALIZADO.
+# Definir índice normalizado.
 
 import sys, math, random, datetime, Image
 import numpy as np
@@ -87,56 +94,79 @@ if __name__ == '__main__':
 			alls[obj[trial[2]]] += 1/time
 			alls["TOTAL"] += 1
 			avg_users[int(trial[2])][user] += 1/time
+	colors = ['r','g','b','gold','c','purple','darkorange','darkblue','olive','mediumpurple','lightcoral','yellowgreen','saddlebrown','dodgerblue','lightpink','darkgrey','k']
 
-	colors = ['r','g','b','gold','c','purple','darkorange','dodgerblue','olive','mediumpurple','lightcoral','yellowgreen','darkblue','lightpink','k','darkgrey']
+	names = []
+	ir = []
 
 	pTrials = False
 #	pTrials = True
-	if (pTrials):
-		for t in trials_c:
-			highs = [0,0,0,0]
-			c = 0
-			gs = []
-			legends = []
-			for h in trials_c[t]:
-				tp = trials_c[t][h][2]
-				k = 1.0 * trials_c[t][h][1] / trials_c[t][h][0]
-				if (tp == '0'):
-					legends.append(h)
-					gs.append(plt.bar((0,1,2,3), (k,0,0,0),0.2,bottom=highs,color=colors[c]))
-					highs[0] += k
-				elif (tp == '1'):
-					legends.append(h)
-					gs.append(plt.bar((0,1,2,3), (0,k,0,0),0.2,bottom=highs,color=colors[c]))
-					highs[1] += k
-				elif (tp == '2'):
-					legends.append(h)
-					gs.append(plt.bar((0,1,2,3), (0,0,k,0),0.2,bottom=highs,color=colors[c]))
-					highs[2] += k
-				elif (tp == '3'):
-					legends.append(h)
-					gs.append(plt.bar((0,1,2,3), (0,0,0,k),0.2,bottom=highs,color=colors[c]))
-					highs[3] += k
-				c = c+1
-				if c >= len(colors):
-					c = 0
-			plt.xticks((0,1,2,3),(obj["0"],obj["1"],obj["2"],obj["3"]))
-			plt.title(u'Decisiones en el trial ' + t)
+	for t in trials_c:
+		highs = [0,0,0,0]
+		c = 0
+		gs = []
+		legends = []
+		names.append(t)
+		for h in trials_c[t]:
+			tp = trials_c[t][h][2]
+			# k = Ir_t de h / cant apariciones de h en t
+			k = 1.0 * trials_c[t][h][1] / trials_c[t][h][0]
+			if (tp == '0'):
+				legends.append(h)
+				if (pTrials):
+					gs.append(plt.bar((-0.1,0.9,1.9,2.9), (k,0,0,0),0.2,bottom=highs,color=colors[c]))
+				highs[0] += k
+			elif (tp == '1'):
+				legends.append(h)
+				if (pTrials):
+					gs.append(plt.bar((-0.1,0.9,1.9,2.9), (0,k,0,0),0.2,bottom=highs,color=colors[c]))
+				highs[1] += k
+			elif (tp == '2'):
+				legends.append(h)
+				if (pTrials):
+					gs.append(plt.bar((-0.1,0.9,1.9,2.9), (0,0,k,0),0.2,bottom=highs,color=colors[c]))
+				highs[2] += k
+			elif (tp == '3'):
+				legends.append(h)
+				if (pTrials):
+					gs.append(plt.bar((-0.1,0.9,1.9,2.9), (0,0,0,k),0.2,bottom=highs,color=colors[c]))
+				highs[3] += k
+			c = c+1
+			if c >= len(colors):
+				c = 0
+		ir.append(highs[0])
+		if (pTrials):
+			plt.xticks((-0.5,0,1,2,3,3.5),("",obj["0"],obj["1"],obj["2"],obj["3"],""))
+			plt.title(u'Índice de relación relativa por choice del target ' + t,fontsize=20)
+			plt.ylabel(u'Índice de relación relativa',fontsize=20)
 			plt.legend(gs,legends,bbox_to_anchor=(1.1, 1.05))
+			plt.grid()
 			plt.show()
+
+	pTarget = False
+	pTarget = True
+	if (pTarget):
+		plt.xticks(range(len(names)),names)
+		plt.bar(range(len(names)),ir)
+		plt.show()
 
 	pTusers = False
 #	pTusers = True
+	c = 0
 	if (pTusers):
 		for u in temp_users:
 			if len(u) > 0:
-				plt.plot(u,'-o')
-				plt.yticks((0,1,2,3),(obj["0"],obj["1"],obj["2"],obj["3"]))
-				plt.title(u'Decisiones de un usuario en función del tiempo')
+				plt.plot(u,'-o',color=colors[c])
+				plt.yticks((-0.5,0,1,2,3,3.5),("",obj["0"],obj["1"],obj["2"],obj["3"],""))
+				plt.title(u'Decisiones de un usuario en función del tiempo',fontsize=20)
+				plt.xlabel(u'Número de trial',fontsize=20)
 				plt.show()
+				c = c+1
+				if c >= len(colors):
+					c = 0
 
 	pUsers = False
-	pUsers = True
+#	pUsers = True
 	if (pUsers):
 		c = 0
 		l = 1.0/(len(users)+10)
@@ -145,8 +175,10 @@ if __name__ == '__main__':
 				#p = plt.plot((0+u*l,1+u*l,2+u*l,3+u*l),(users[u][obj['0']],users[u][obj['1']],users[u][obj['2']],users[u][obj['3']]),colors[c])
 				p = plt.bar((0+u*l,1+u*l,2+u*l,3+u*l),(users[u][obj['0']],users[u][obj['1']],users[u][obj['2']],users[u][obj['3']]),width=l,color=colors[c])
 				#p = plt.bar((0+u*l,1+u*l,2+u*l,3+u*l),(users[u][obj['0']]/users[u]["TOTAL"],users[u][obj['1']]/users[u]["TOTAL"],users[u][obj['2']]/users[u]["TOTAL"],users[u][obj['3']]/users[u]["TOTAL"]),width=l,color=colors[c])
-				plt.title(u'Decisiones de los usuarios')
-				plt.ylabel("Indice de relacion relativa")
+				plt.title(u'Índice de relación de los usuarios por cada clase',fontsize=20)
+				#plt.title(u'Índice de relación relativa de los usuarios por cada clase',fontsize=20)
+				plt.ylabel(u'Índice de relación',fontsize=20)
+				#plt.ylabel(u'Índice de relación relativa',fontsize=20)
 				plt.xticks((0+l*len(users)/2,1+l*len(users)/2,2+l*len(users)/2,3+l*len(users)/2),(obj["0"],obj["1"],obj["2"],obj["3"]))
 				c = c+1
 				if c >= len(colors):
@@ -154,7 +186,7 @@ if __name__ == '__main__':
 		plt.show()
 
 	pGlobal = False
-	pGlobal = True
+#	pGlobal = True
 	if (pGlobal):
 #		plt.bar((0,1,2,3),(alls[obj['0']],alls[obj['1']],alls[obj['2']],alls[obj['3']]),0.2)
 #		plt.title(u'Decisiones conjuntas de todos los trials y todos los usuarios')
@@ -190,6 +222,6 @@ if __name__ == '__main__':
 	z += ")"
 #	print z
 	full = sorted(full, key=lambda x : x[2])
-	for r in full:
-		if (r[2] > 0):
-			print names[r[0]] + " -> " + names[r[1]] + ": " + str(r[2])
+	#for r in full:
+	#	if (r[2] > 0):
+	#		print names[r[0]] + " -> " + names[r[1]] + ": " + str(r[2])
